ac558134cbea3beefe08dc058c24813b162db985
harsh2602 <harsh2602@gmail.com>
2015-06-21 18:37:49 +0530

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function is used to cache a vector and a matrix that will be used later to evaluate inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -17,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function is used to create the inverse of a matrix that has been cached using the cacheMatrix function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
